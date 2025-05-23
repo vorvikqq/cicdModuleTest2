5d4126da867242c617bc6ce4c82db4241a42c73e
vorvikqq <delodonn@ukr.net>
2025-05-23 15:44:42 +0300

    add image_detail to see image by id

--- gallery/views.py
+++ gallery/views.py
@@ -5,3 +5,7 @@ def gallery_view(request):
     categories = Category.objects.all()
     return render(request, 'gallery.html', {'categories': categories})
 
+
+def image_detail(request, pk):
+    image = get_object_or_404(Image, id=pk)
+    return render(request, 'image_detail.html', {'image': image})
